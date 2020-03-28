702ec9568b4351bca73129b495e426641767df08
Kelvin12-bit <62745367+Kelvin12-bit@users.noreply.github.com>
2020-03-28 15:06:45 -0700

    Add files via upload

---
+++ Project2.py
@@ -0,0 +1,5 @@
+# This program takes details from patients.
+myName=input('What is your name?')
+myAge=input('What is your age?')
+PlaceOfResidence=input('Where do you reside?') 
+HealthSymptoms=input('What are the health symptoms you are facing?')
